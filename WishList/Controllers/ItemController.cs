d47b8bd3251a6ff6fda383c973ecc9815099a1a2
Daniel <ll8083129@gmail.com>
2020-09-22 15:36:26 +0200

    1

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -41,14 +41,9 @@ namespace WishList.Controllers
         public IActionResult Delete(int Id)
         {
             var item = _context.Items.SingleOrDefault(i => i.Id == Id);
-            if (item != null)
-            {
-                _context.Items.Remove(item);
+            _context.Items.Remove(item);
                 _context.SaveChanges();
                 return RedirectToAction("Index");
-            }
-
-            return NotFound();
         }
     }
 }
